6be8349f8a05cfbb5c0f81897a61e7e83779576c
Augusto Lopes <augustolfp@gmail.com>
2022-08-06 12:36:50 -0300

    Adds password encryption at user Creation routine

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,19 @@
 import connection from "../database/database.js";
+import bcrypt from 'bcrypt';
+import { newUserSchema } from "../schemas/authSchemas.js";
 
 export async function createUser(req, res) {
     const newUser = req.body;
+    const validation = newUserSchema.validate(newUser);
+
+    if(validation.error) {
+        return res.status(422).send(validation.error.details);
+    }
+
+
+    const encryptedPass = bcrypt.hashSync(newUser.password, 10);
     try {
-        const query = await connection.query('INSERT INTO users ("name", "email", "password") VALUES ($1,$2,$3)',[newUser.name,newUser.email,newUser.password]);
+        const query = await connection.query('INSERT INTO users ("name", "email", "password") VALUES ($1,$2,$3)',[newUser.name,newUser.email,encryptedPass]);
         return res.sendStatus(201);
     }
     catch(error) {
