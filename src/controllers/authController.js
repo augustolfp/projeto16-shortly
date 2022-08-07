719b27947eb6a4f1d66a046004abd538d9d946c9
Augusto Lopes <augustolfp@gmail.com>
2022-08-07 13:12:59 -0300

    Adds error handling on signup endpoint

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,13 +10,16 @@ export async function createUser(req, res) {
         return res.status(422).send(validation.error.details[0].message);
     }
 
-
     const encryptedPass = bcrypt.hashSync(newUser.password, 10);
-    try {
-        const query = await connection.query('INSERT INTO users ("name", "email", "password") VALUES ($1,$2,$3)',[newUser.name,newUser.email,encryptedPass]);
-        return res.sendStatus(201);
-    }
-    catch(error) {
-        return res.send(error).status(400);
-    }
+    const query = connection.query('INSERT INTO users ("name", "email", "password") VALUES ($1,$2,$3)',[newUser.name,newUser.email,encryptedPass]);
+
+    query.then(() => {
+        res.sendStatus(201);
+        return;
+    });
+
+    query.catch((error) => {
+        res.status(409).send(error);
+        return;
+    });
 }
